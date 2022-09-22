cadb90915bb1a03d3a43bed92e3a426407793347
marina-zadoianchuk <93602034+marina-zadoianchuk@users.noreply.github.com>
2022-09-22 14:20:32 +0300

    lesson6

--- src/main/java/ru/gb/lesson6/PantsPage.java
+++ src/main/java/ru/gb/lesson6/PantsPage.java
@@ -1,9 +1,11 @@
 package ru.gb.lesson6;
 
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -13,13 +15,14 @@ public class PantsPage extends BaseView {
         super(driver);
       //  this.categoryList = categoryList;
     }
-    @FindBy(xpath = "//div[contains(@data-filter-name, 'xsubject')]")
+    private static final String categoryListXpath = "//fieldset[@class='j-list filter__fieldset list_left_xsubject render_type_1 filter__fieldset--limited']/label";
+    @FindBy(xpath = categoryListXpath)
     private List<WebElement> categoryList;
 
     public PantsPage selectCategory(String category) throws InterruptedException {
-        Thread.sleep(15000); //ожидание для дебага
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated((By.xpath(categoryListXpath))));
         categoryList.stream().filter(d -> d.getText().contains(category)).findFirst().get().click();
-        Thread.sleep(15000); //ожидание для дебага
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated((By.xpath(categoryListXpath))));
         Assertions.assertTrue(driver.getCurrentUrl().contains(pantsCategoryUrl));
         return new PantsPage(driver);
     }
